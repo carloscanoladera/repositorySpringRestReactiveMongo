714dd0263901348deaaa4986faa83e2111f4b5b3
carloscanoladera <carlos.cano.ladera@outlook.es>
2022-04-16 15:40:10 +0200

    Testing the get Method

--- rest-mvc-customers-mongoreact/src/test/java/com/example/eclipse/spring5mvcmongoreact/controllers/CustomerControllerReactiveTest.java
+++ rest-mvc-customers-mongoreact/src/test/java/com/example/eclipse/spring5mvcmongoreact/controllers/CustomerControllerReactiveTest.java
@@ -7,21 +7,27 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.reactivestreams.Publisher;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 import com.example.eclipse.spring5mvcmongoreact.api.model.Customer;
 import com.example.eclipse.spring5mvcmongoreact.api.repositories.CustomerReactiveRepository;
 import com.example.eclipse.spring5mvcmongoreact.controller.CustomerControllerReactive;
 import com.example.eclipse.spring5mvcmongoreact.services.CustomerService;
+import com.example.eclipse.spring5mvcmongoreact.services.CustomerServiceImpl;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 class CustomerControllerReactiveTest {
 
 	@Mock
 	CustomerReactiveRepository customerReactiveRepository;
-	@InjectMocks
+	
 	CustomerService customerService;
+	
 	CustomerControllerReactive customerController;
 	
 	 WebTestClient webTestClient;
@@ -29,17 +35,22 @@ class CustomerControllerReactiveTest {
 	@BeforeEach
     public void setUp() throws Exception {
      
+		customerReactiveRepository= Mockito.mock(CustomerReactiveRepository.class);
+		
+		customerService  = new CustomerServiceImpl(customerReactiveRepository); 
+		customerController = new CustomerControllerReactive(customerService);
+		
         webTestClient = WebTestClient.bindToController(customerController).build();
     }
 
     @Test
     public void list() {
-        given(customerReactiveRepository.findAll())
+        given(customerService.getAllCustomers())
                 .willReturn(Flux.just(new Customer(1L,"Custom1","Lastname1"),
                 		new Customer(2L,"Custom2","Lastname2")));
 
         webTestClient.get()
-                .uri("/api/v2/customer/")
+                .uri("/api/v2/customers/")
                 .exchange()
                 .expectBodyList(Customer.class)
                 .hasSize(2);
